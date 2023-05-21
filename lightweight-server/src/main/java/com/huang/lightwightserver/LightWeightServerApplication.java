8b6017c6243af5a132c83bddbdc9b67c820bf962
handsomehuang <huangjg>
2023-05-21 21:20:39 +0800

    inital commit

---
+++ lightweight-server/src/main/java/com/huang/lightwightserver/LightWeightServerApplication.java
@@ -0,0 +1,13 @@
+package com.huang.lightwightserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightWightServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightWightServerApplication.class, args);
+    }
+
+}
---
+++ lightweight-server/src/test/java/com/huang/lightwightserver/LightWeightServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huang.lightwightserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightWightServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
